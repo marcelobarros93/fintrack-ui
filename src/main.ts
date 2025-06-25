f33253ad1741ba681b24a78c5fd590ad1c9a2067
Marcelo Barros <marcelobarros-sms@hotmail.com>
2025-06-24 21:44:55 -0300

    refactor: migrate to standalone AppComponent and remove AppModule

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,55 @@
-import { enableProdMode } from '@angular/core';
+import {
+  enableProdMode,
+  DEFAULT_CURRENCY_CODE,
+  LOCALE_ID,
+  importProvidersFrom,
+} from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { ToastModule } from 'primeng/toast';
+import { AppRoutingModule } from './app/app-routing.module';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { FormBuilder } from '@angular/forms';
+import { MessageService, ConfirmationService } from 'primeng/api';
+import { DatePipe, CommonModule, registerLocaleData } from '@angular/common';
+import { AuthInterceptor } from './app/auth.interceptor';
+import {
+  HTTP_INTERCEPTORS,
+  withInterceptorsFromDi,
+  provideHttpClient,
+} from '@angular/common/http';
+
+import localePt from '@angular/common/locales/pt';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+registerLocaleData(localePt, 'pt-BR');
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      BrowserModule,
+      AppRoutingModule,
+      CommonModule,
+      ToastModule
+    ),
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+    DatePipe,
+    MessageService,
+    ConfirmationService,
+    FormBuilder,
+    provideAnimations(),
+    provideHttpClient(withInterceptorsFromDi()),
+  ],
+}).catch((err) => console.error(err));
